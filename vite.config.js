8ff0154a3c2b6d6f0917ec2db9f71acb4653b9ae
sttadic <stjepan.tadich@gmail.com>
2025-02-17 20:24:37 +0000

    Set up configuration to deploy using github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/e-plantShopping-react",
   plugins: [react()],
   server: {
     hmr: true,
