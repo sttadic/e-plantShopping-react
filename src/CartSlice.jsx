274a57249c398a3170ee8a64619ba943143822bd
sttadic <stjepan.tadich@gmail.com>
2025-02-17 12:48:33 +0000

    Add logic for removeItem and updateQuantity reducers in CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -15,8 +15,14 @@ export const CartSlice = createSlice({
 				state.items.push({ name, image, cost, quantity: 1 });
 			}
 		},
-		removeItem: (state, action) => {},
-		updateQuantity: (state, action) => {},
+		removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
+    },
+		updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) itemToUpdate.quantity = quantity;
+    },
 	},
 });
 
