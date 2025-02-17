90f0be7cffd6c8b3b5a2f667fa9beaf426299449
sttadic <stjepan.tadich@gmail.com>
2025-02-17 12:15:34 +0000

    Implement addItem reducer

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isPlainObject } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
+	name: "cart",
+	initialState: {
+		items: [], // Initialize items as an empty array
+	},
+	reducers: {
+		addItem: (state, action) => {
+			const { name, image, cost } = action.payload;
+			const existingItem = state.items.find((item) => item.name === name);
+			if (existingItem) {
+				existingItem.quantity++;
+			} else {
+				state.items.push({ name, image, cost, quantity: 1 });
+			}
+		},
+		removeItem: (state, action) => {},
+		updateQuantity: (state, action) => {},
+	},
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
